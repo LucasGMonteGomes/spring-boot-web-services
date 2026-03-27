883d9fb210899fa52628a48d65eca318d4c1e37b
LucasGMonteGomes <218098777+LucasGMonteGomes@users.noreply.github.com>
2026-03-27 19:35:38 -0300

    Project created

---
+++ src/main/java/com/krd/SpringBootWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.krd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebServicesApplication.class, args);
+	}
+
+}
