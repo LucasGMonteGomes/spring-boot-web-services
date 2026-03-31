56393ac774188b5ece038a7800b1cbeb559d17d0
LucasGMonteGomes <218098777+LucasGMonteGomes@users.noreply.github.com>
2026-03-31 16:44:54 -0300

    Subtotal & Total methods

--- src/main/java/com/krd/entities/Order.java
+++ src/main/java/com/krd/entities/Order.java
@@ -98,4 +98,12 @@ public class Order implements Serializable {
     public int hashCode() {
         return Objects.hashCode(getId());
     }
+
+    public Double getTotal() {
+        double sum = 0.0;
+        for (OrderItem x : items) {
+            sum += x.getSubTotal();
+        }
+        return sum;
+    }
 }
--- src/main/java/com/krd/entities/OrderItem.java
+++ src/main/java/com/krd/entities/OrderItem.java
@@ -71,4 +71,8 @@ public class OrderItem implements Serializable {
     public int hashCode() {
         return Objects.hashCode(id);
     }
+
+    public Double getSubTotal(){
+        return getPrice() * getQuantity();
+    }
 }
