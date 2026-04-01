b3a157b6c9d94bc2a932b03ad957f1a79907ab14
LucasGMonteGomes <218098777+LucasGMonteGomes@users.noreply.github.com>
2026-04-01 15:06:35 -0300

    Secure user passwords with BCrypt hashing

--- src/main/java/com/krd/entities/User.java
+++ src/main/java/com/krd/entities/User.java
@@ -18,6 +18,7 @@ public class User implements Serializable {
     private String name;
     private String email;
     private String phone;
+    @JsonIgnore
     private String password;
 
     @JsonIgnore
